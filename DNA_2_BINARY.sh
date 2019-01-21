74d8d614dfb2a93caf9c0392e1429218a1f29033
Jonas Ohlsson <jonas@vansinne.se>
2019-01-21 21:35:36 +0100

    Add proper quoting to allow for spaces in filenames.

--- DNA_2_BINARY.sh
+++ DNA_2_BINARY.sh
@@ -8,10 +8,10 @@ echo "
 please provide the name of your fasta file"
 read filename
 fasta_file=filename
-cut -d ' ' -f1 ${!fasta_file} > ${!fasta_file}_trimhead
-awk 'BEGIN{RS=">"}NR>1{sub("\n","\t"); gsub("\n",""); print RS$0}' ${!fasta_file}_trimhead > ${!fasta_file}_tab
-cat ${!fasta_file}_tab | awk '{print $1}' > ${!fasta_file}_tab_col1
-cat ${!fasta_file}_tab | awk '{print $2}' > ${!fasta_file}_tab_col2
+cut -d ' ' -f1 "${!fasta_file}" > "${!fasta_file}"_trimhead
+awk 'BEGIN{RS=">"}NR>1{sub("\n","\t"); gsub("\n",""); print RS$0}' "${!fasta_file}"_trimhead > "${!fasta_file}"_tab
+awk '{print $1}' < "${!fasta_file}"_tab > "${!fasta_file}"_tab_col1
+awk '{print $2}' < "${!fasta_file}"_tab > "${!fasta_file}"_tab_col2
 echo "
 -----------------------------------
 The codes used for translation are:
@@ -20,10 +20,10 @@ The codes used for translation are:
             C=01
                 G=10
 -----------------------------------"
-sed 's/A/00/g;s/T/11/g;s/C/01/g;s/G/10/g' ${!fasta_file}_tab_col2 > ${!fasta_file}_tab_col2_2
-paste ${!fasta_file}_tab_col1 ${!fasta_file}_tab_col2_2 | column -s $'\n' -t > ${!fasta_file}_binary.txt
-sed -i 's/\t/\n/g' ${!fasta_file}_binary.txt
-rm -f ${!fasta_file}_trimhead ${!fasta_file}_tab ${!fasta_file}_tab_col1 ${!fasta_file}_tab_col2 ${!fasta_file}_tab_col2_2
+sed 's/A/00/g;s/T/11/g;s/C/01/g;s/G/10/g' "${!fasta_file}"_tab_col2 > "${!fasta_file}"_tab_col2_2
+paste "${!fasta_file}"_tab_col1 "${!fasta_file}"_tab_col2_2 | column -s $'\n' -t > "${!fasta_file}"_binary.txt
+sed -i 's/\t/\n/g' "${!fasta_file}"_binary.txt
+rm -f "${!fasta_file}"_trimhead "${!fasta_file}"_tab "${!fasta_file}"_tab_col1 "${!fasta_file}"_tab_col2 "${!fasta_file}"_tab_col2_2
 echo "                            ========================="
 echo "your result file is ---->>> ${!fasta_file}_binary.txt"
 echo "                            ========================="
